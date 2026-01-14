7f737ac091cf93857d525739419bfc4ecdab7412
Vishnu-Ai-Dev <vishnuravi226@gmail.com>
2026-01-15 00:38:38 +0530

    Day 7 -condtional and login system

---
+++ day_7/conditional.py
@@ -0,0 +1,45 @@
+#starting of the day
+print("day-7 loops started")
+
+#using the condtionals operation is else trying to open the logic gates order 
+
+'''print("       APPLICATION TO BE ASSISTANT OF WORLD GREATEST DETECTIVE RANPO  ")
+enter_name=input("enter your name:")
+cases_solved=int(input("number of cases solved:"))
+if cases_solved>1000:
+    print("you are qualified to be the assistant of Ranpo please wait for the call")
+elif cases_solved>=500:
+    print("your application has been accepted for the future purpose")
+else:
+    print("you are not eligible")'''
+
+#using the conditonal loops with third condition
+'''print("       APPLICATION TO BE ASSISTANT OF WORLD GREATEST DETECTIVE RANPO  ")
+enter_name=input("enter your name:")
+cases_solved=int(input("number of cases solved:"))
+has_ability=input("Do you have speacial ability(y/n):")
+if cases_solved>1000 and has_ability=="y":
+    print("you are qualified to be the assistant of Ranpo please wait for the call")
+elif cases_solved>=500 and has_ability=="y":
+    print("your application has been accepted for the future purpose")
+else:
+    print("you are not eligible")'''
+
+#using the condtional loops creating a login system for the   
+
+print("          welcome to the login page       ")
+user_name=input("enter your user name:")
+password=input("enter your password:")
+if user_name=="gilgamesh":
+    print("user is banned")
+elif user_name=="archer" and password=="emiya_shirou_97":
+    print("Admin detected! The system enters into admin mode")
+elif user_name=="saber" and password=="king_arthor_97":
+    print("you are an normal user you can enter the website")
+elif user_name=="saber" and password!="king_arthor_97":
+    print("incorrect password")
+else:
+    print("no user detected please sign up with a new account")
+
+
+
